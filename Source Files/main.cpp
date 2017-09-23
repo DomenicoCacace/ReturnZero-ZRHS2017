5af12e3be630c6a1b17496a6cb958034e0acf380
Domenico <cacace.d98@gmail.com>
2017-09-23 18:06:56 +0200

    Adapted main to new functions

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -1,16 +1,6 @@
 /*
 * main.cpp
 *
-*Questo è un main generico, contenente le funzioni di base del sistema
-*e le variabili globali necessarie per utilizzare tutte le funzioni
-*
-*Da questa branch si consiglia di creare un fork da poi editare, per mantenere
-*un programma ben strutturato adatto ad ogni competizione
-*
-*Nel caso vi domandaste perché in alcuni passaggi di parametri sia indicato
-* *var e non var[], sappiate che siete delle cattive persone che non dovrebbero
-*mettere le mani sul codice.
-*
 */
 
 //VARIABILI GLOBALI
@@ -21,12 +11,17 @@ float   myState[12];    //utilizzato per lavorare su getMyZRState, contiene tutt
 float   myPos[3];       //contiene le coordinate del satellite al tempo dell'ultima chiamata alla funzione getMyPos
 float   myAtt[3];       //contiene l'orientamento del satellite al tempo dell'ultima chiamata alla funzione getMyAtt
 
+//variabili utili alle funzioni di rotazione
 float   virtualPoint;   //utilizzata per creare un punto virtuale per la rotazione sull'asse Z di 180°
 
 //Variabili varie: non compongono una classe specifica di variabili, ma servono. Molto.
 float   approximation;  //contiene il margine di approssimazione generico (vedi misc.cpp -> compareVector(args))
 float   brakeDist;      //contiene la distanza dalla quale il satellite inizia a frenare (vedi movement.cpp -> goToPos())
 char    index;          //utilizzato per scorrere tra i case dello switch che  ̶p̶r̶o̶b̶a̶b̶i̶l̶m̶e̶n̶t̶e̶ sicuramente sarà necessario
+
+//variabili di gioco
+
+analyzerPos[3];     //coordinate dell'analizzatore
 
 
 //INIZIALIZZAZIONE
